a0286f91dcb646eb2d79efec7f76ad9f4a494ad7
Gergo Koos <gergo@koosgergo.info>
2013-09-11 14:43:54 +0100

    First release
    Supports only the jpg extension
    Added some UI buttons

--- sgal.php
+++ sgal.php
@@ -1,12 +1,15 @@
 <?php
 	// sgal - Simple Galery
 	// made by Gergo Koos
-	// licensed under Creative Commons Attribution-NonCommercial 3.0 Unported License.
+	// http://koosgergo.info
+	// gergo@koosgergo.info
+	// licensed under Creative Commons Attribution-NonCommercial 3.0 Unported License with an addition of: if you modificate the program, you have to share with me
+	//http://creativecommons.org/licenses/by-nc/3.0/
 	
 	
 	//-------- SETTINGS ----------
 	
-	$filetypes = array('jpg', 'png');
+	$filetypes = array('jpg'); // currently only jpg files
 	$thumbnailsize = '200'; // longer side in pixels, if you change this, you have to delete manually the already existing thumbnails (files starting with _)
 	$margin = '50'; // in px;
 	
@@ -60,25 +63,24 @@
 	}
 	else {
 		if ($handle = opendir(getcwd())) {
-	        while (($file = readdir($handle)) !== false) { 
-	            if ( in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), $filetypes) and substr($file, 0,1) != '_' ) {
-		           
-		            list($width, $height) = getimagesize($file);
-		            $pictures[] = array('file' => $file,
-		            					'width' => $width,
-		            					'height' => $height);
-		            $piclist[] = $file;
-
-	            }
-	        }
-	        closedir($handle);
-	    }
-	    sort($pictures);
-	    sort($piclist);
-	    $showhtml = true;
+			$pictures = NULL;
+			while (($file = readdir($handle)) !== false) { 
+				if ( in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), $filetypes) and substr($file, 0,1) != '_' ) {
+					list($width, $height) = getimagesize($file);
+					$pictures[] = array('file' => $file,
+										'width' => $width,
+										'height' => $height);
+					$piclist[] = $file;
+				}
+			}
+			closedir($handle);
+		}
+		if ( count($pictures) > 0 ) {
+			sort($pictures);
+			sort($piclist);
+		} 
+		$showhtml = true;
 	}
-	
-
 ?>
 <?php if ($showhtml) : ?>
 <!DOCTYPE html>
@@ -101,30 +103,50 @@
 				imgRatio = 0;
 			
 			jQuery(document).ready(function($) { 
-				
+	
 				vportWidth = $(window).width();
 				vportHeight = $(window).height();
 				
 				$('#overlay').css('height',	vportHeight+'px'); // setting up the overlay height, width is static
 				
-			
+				// click events
 				$('#overlay').click(function() {
+					$("body").css('overflow','auto');
 					$("#overlay").hide();
 					$("#wrapper").hide();
 				});
+				
+				$('#previmg').click(function() {
+					var imgnum = jQuery.inArray($("#bigpic").attr('src'), imglist)-1;
+					if ( imgnum >= 0) {
+						openThumb(imgnum);
+					}
+					
+				});
+				
+				$('#nextimg').click(function() {
+					var imgnum = jQuery.inArray($("#bigpic").attr('src'), imglist)+1;
+					if ( imgnum < imglist.length) {
+						openThumb(imgnum);
+					}
+				});
+				
+				$('#closeimg').click(function() {
+					$('#overlay').click();
+				});
+				
 			
 				$('.thumbnail').click(function() { 
-					//<img id="bigpic" src="DSCF9106.JPG" />
-					//$("#boxcontent").html('');
-					
-					
 					openThumb($(this).attr('id'));
 					
+					$("#overlay").css('top',$(document).scrollTop());
 					$("#overlay").show();
 					$("#wrapper").show();
 					$("#bigcontout").fadeIn();
 					
-				});		
+				});	
+				
+				// keypress events
 				
 				$("body").keydown(function(event) {
 					if(event.keyCode == 39) { //right
@@ -142,57 +164,52 @@
 					}
 					
 					if(event.keyCode == 37) { //left					
-						
-						//alert(jQuery.inArray('DSCF8530.JPG', images));
 						var imgnum = jQuery.inArray($("#bigpic").attr('src'), imglist)-1;
 						if ( imgnum >= 0) {
-							//$("#boxcontent").html('<img id="bigpic" src="'+imglist[imgnum]+'" />');
 							openThumb(imgnum);
-						}
-						
-						
+						}		
 					}
 					
 					if(event.keyCode == 39) { //right
 						var imgnum = jQuery.inArray($("#bigpic").attr('src'), imglist)+1;
 						if ( imgnum < imglist.length) {
-							//$("#boxcontent").html('<img id="bigpic" src="'+imglist[imgnum]+'" />');
 							openThumb(imgnum);
 						}
-												
-						
 					}	
 				});	
-			});
+			}); // end of document ready
 			
+			
+			// functions
 			function setWrapperSize(width,height) {
 				$("#wrapper").css('width',width); 
 				$("#wrapper").css('height',height);
 
 				$("#wrapper").css('left',(vportWidth/2)-(width/2));
-				$("#wrapper").css('top',(vportHeight/2) - (height/2));
+				$("#wrapper").css('top',(vportHeight/2) - (height/2)+$(document).scrollTop());
 			}
 			
 			function openThumb(id) {
 			
-				$("#boxcontent").html('<img id="bigpic" src="'+$("#"+id).data('file')+'" />');
+				$("#boxcontent").html('<img id="bigpic" src="'+$("#"+id).data('file')+'" alt="" />');
+				$("body").css('overflow','hidden');
 				
-					imgWidth = $("#"+id).data('width'),
-					imgHeight = $("#"+id).data('height'),
-					imgRatio = imgWidth/imgHeight;
-					
-					if (imgRatio > 1 ) {
-						//landscape
-						if (imgHeight > vportHeight ) { //in case the image is taller than the screen
-							setWrapperSize(imgRatio*(vportHeight-margin),(vportHeight-margin));
-						}
-						else setWrapperSize(imgWidth,imgHeight);
-						
-					}
-					else {
-						//portrait
+				imgWidth = $("#"+id).data('width'),
+				imgHeight = $("#"+id).data('height'),
+				imgRatio = imgWidth/imgHeight;
+				
+				if (imgRatio > 1 ) {
+					//landscape
+					if (imgHeight > vportHeight ) { //in case the image is taller than the screen
 						setWrapperSize(imgRatio*(vportHeight-margin),(vportHeight-margin));
 					}
+					else setWrapperSize(imgWidth,imgHeight);
+					
+				}
+				else {
+					//portrait
+					setWrapperSize(imgRatio*(vportHeight-margin),(vportHeight-margin));
+				}
 			}
 			
 		//]]>
@@ -200,7 +217,7 @@
 	<style>
 		body {
 			background-color: rgba(0,0,0,0.8);
-			overflow-x: hidden;
+			
 		}
 		#container {
 			text-align: center;
@@ -227,52 +244,23 @@
 			width: 100%;
 			height: 100%;
 			background-color: black;
-			
-		
 		}
 		
-		/*------*/
-		
-		#bigcontin {
-			position: relative;
-			left: -50%;
-			border: 1px solid red;
-			
-		}
-		#bigcontout {
-			display: none;
-			position: absolute;
-			left: 50%;
-			top: 10px;
-			width: 90%;
-			height: 90%;
-			border: 1px solid white;
-			 
-		}
-		
-		
-		/*------*/
-		
-		#overlay {
-			
+		#overlay {		
 			display: none;
 			cursor: pointer;
 			position: absolute;
-			top: 0px;
 			left: 0px;
 			width: 100%;
 			opacity: 0.7;
 			z-index: 1000;
 			background-color: rgb(129,129,129);
-			border: 0px solid red;
-			
 		}
 		
 		#wrapper {
 			display: none;
 			position: absolute;
-			border: 1px solid red;
-			background-color: white;
+			border: 1px solid #cccccc;
 			z-index: 1001;
 		}
 		
@@ -288,24 +276,74 @@
 			max-height: 100%;
 		}
 		
+		#previmg, #nextimg {
+			position: absolute;
+			height: 100%;
+			font-size: 50px;
+			color: white;
+			text-shadow: 2px 2px 2px #000000;
+			cursor: pointer;
+		}
 		
+		.vmiddle {
+			position: relative;
+			top:45%;
+		}
+		
+		#previmg {
+			left: 0px;
+			padding-left: 30px;
+			padding-right:30px;
+		}
+		
+		#nextimg {
+			right: 0px;
+			padding-left: 30px;
+			padding-right:30px;
+		}
+		
+		#closeimg {
+			position: absolute;
+			height: 32px;
+			right: -18px;
+			top: -18px;
+			font-size: 40px;
+			color: white;
+			font-family: helvetica;
+			border: 4px solid white;
+			border-radius: 50px;
+			box-shadow: 2px 2px 5px #000000;
+			cursor: pointer;
+		}
+		
+		#noimg {
+			color: #dddddd;
+			font-size: 40px;
+			font-family: helvetica;
+			margin-top: 100px;
+		}
 	</style>
 	
 </head>
 <body>
 
 <div id="container">
-	<?php for ($i=0;$i<count($pictures);$i++) : ?>
-		<span class="thumbnail" id="<?php echo $i; ?>" data-file="<?php echo $pictures[$i]['file']; ?>" data-width="<?php echo $pictures[$i]['width']; ?>" data-height="<?php echo $pictures[$i]['height']; ?>" ><img src="<?php echo $_SERVER['PHP_SELF'].'?t='.$pictures[$i]['file']; ?>" alt=""/></span>
-	<?php endfor; ?>
 	
+	<?php if ( count($pictures) > 0 ) : for ($i=0;$i<count($pictures);$i++) :?>
+		<span class="thumbnail" id="<?php echo $i; ?>" data-file="<?php echo $pictures[$i]['file']; ?>" data-width="<?php echo $pictures[$i]['width']; ?>" data-height="<?php echo $pictures[$i]['height']; ?>" ><img src="<?php echo $_SERVER['PHP_SELF'].'?t='.$pictures[$i]['file']; ?>" alt=""/></span>
+		<?php endfor; ?>
+	
+	<?php else:	?>
+	<div id="noimg">No photos in this folder:-(</div>
+	<?php endif; ?>
 </div>
 
 <div id="overlay"></div>
 <div id="wrapper">
-	<div id="boxcontent">
-		&nbsp;
-	</div>
+	<span id="previmg"><span class="vmiddle">&lt;</span></span>
+	<span id="nextimg"><span class="vmiddle">&gt;</span></span>
+	<span id="closeimg">X</span>
+	<div id="boxcontent">&nbsp;</div>
 </div>
 
 </body>
